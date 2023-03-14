44b9163792fd577f1bd4a688fabedf8dac6ffb6e
Juan Pedro Armella <juanpedroarmella@gmail.com>
2023-03-14 20:56:37 -0300

    fix(app): imgs routes

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export class Index {
   private static canvas: HTMLCanvasElement = document.getElementById(
     "myCanvas"
   ) as HTMLCanvasElement;
-  private static imgTablero: string = "./../public/celda.png";
+  private static imgTablero: string = "/public/celda.png";
 
   constructor() {
     new MainMenu(Index.canvas as HTMLCanvasElement, Index.imgTablero);
